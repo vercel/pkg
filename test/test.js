3b25eff9a79de729c33a836900a729c06644ada8
igorklopov <igor@klopov.com>
2016-08-25 11:02:06 +0300

    force specifying a target when running tests

--- test/test.js
+++ test/test.js
@@ -2,11 +2,17 @@
 
 'use strict';
 
-const path = require('path');
+const chalk = require('chalk');
 const globby = require('globby');
+const path = require('path');
 const utils = require('./utils.js');
 
 const target = process.argv[2];
+if (!target) {
+  console.log(`> ${chalk.red('ERR!')} Specify a target 'v4', 'v6' etc`);
+  process.exit(2);
+}
+
 const files = path.join(__dirname, '*/main.js');
 globby.sync(files).sort().some(function (file) {
   file = path.resolve(file);
@@ -16,8 +22,9 @@ globby.sync(files).sort().some(function (file) {
       { cwd: path.dirname(file), stdio: 'inherit' }
     );
   } catch (error) {
-    console.log(file, 'FAILED');
-    throw error;
+    console.log(`> ${chalk.red('ERR!')} ${error.message}`);
+    console.log(`> ${chalk.red('ERR!')} ${file} FAILED`);
+    process.exit(2);
   }
   console.log(file, 'ok');
 });
