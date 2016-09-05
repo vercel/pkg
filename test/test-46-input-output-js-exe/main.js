b2996edd2e4f0b60e5f8ae1b7e0d0454e2017928
igor <igor@klopov.com>
2016-09-05 20:35:25 +0300

    just copy for now

---
+++ test/test-46-input-output-js-exe/main.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const assert = require('assert');
+const utils = require('../utils.js');
+
+assert(!module.parent);
+assert(__dirname === process.cwd());
+
+const target = process.argv[2] || 'latest';
+const input = './test-x-index.js';
+const newcomers = [ 'test-output.exe' ];
+const before = utils.filesBefore(newcomers);
+
+utils.pkg.sync([
+  '--target', target,
+  '--output', 'test-output.exe', input
+]);
+
+utils.filesAfter(before, newcomers);
