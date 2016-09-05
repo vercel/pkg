088cb77b8c4b45d62dc9c660212b5154a8bba4d1
igor <igor@klopov.com>
2016-09-05 23:37:30 +0300

    tests should start with `fetch-all` to pre-download base binaries

---
+++ test/test-42-fetch-all/main.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const fetch = require('pkg-fetch');
+const { need, system } = fetch;
+const { knownPlatforms } = system;
+const items = [];
+
+for (const nodeRange of [ 'node0', 'node4', 'node6' ]) {
+  for (const platform of knownPlatforms) {
+    // const archs = (platform === 'linux' ? knownArchs : [ 'x86', 'x64' ]); // TODO armv6/7?
+    const archs = [ 'x64' ]; // TODO 'x86'
+    for (const arch of archs) {
+      items.push({ nodeRange, platform, arch });
+    }
+  }
+}
+
+let p = Promise.resolve();
+items.forEach((item) => {
+  p = p.then(() => need(item));
+});
+
+p.catch((error) => {
+  if (!error.wasReported) console.log(`> ${error}`);
+  process.exit(2);
+});
