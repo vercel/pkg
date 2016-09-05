777e713cd869d1e86d8551bbdaeba7af812225eb
igor <igor@klopov.com>
2016-09-05 22:25:46 +0300

    removed targets synchronously

--- test/test-46-input-output-exe/main.js
+++ test/test-46-input-output-exe/main.js
@@ -8,13 +8,11 @@ const utils = require('../utils.js');
 assert(!module.parent);
 assert(__dirname === process.cwd());
 
-const target = process.argv[2] || 'latest';
 const input = './test-x-index';
 const newcomers = [ 'test-output.exe' ];
 const before = utils.filesBefore(newcomers);
 
 utils.pkg.sync([
-  '--target', target,
   '--output', 'test-output.exe', input
 ]);
 
