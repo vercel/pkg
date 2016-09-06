8b970b0cee4888dbad725f093eada640addfc78f
igorklopov <igor@klopov.com>
2016-09-06 17:56:37 +0300

    lint

--- test/test-46-input-output-targets-many/main.js
+++ test/test-46-input-output-targets-many/main.js
@@ -8,7 +8,6 @@ const utils = require('../utils.js');
 assert(!module.parent);
 assert(__dirname === process.cwd());
 
-const target = process.argv[2] || 'latest';
 const current = `node${process.version[1]}`;
 const input = './test-x-index';
 let arch = process.arch; // TODO extract arch from `target` once it contains
@@ -23,7 +22,7 @@ const newcomers = [
 const before = utils.filesBefore(newcomers);
 
 utils.pkg.sync([
-  '--target', `linux,osx,win`,
+  '--target', 'linux,osx,win',
   '--output', 'test-output', input
 ]);
 
