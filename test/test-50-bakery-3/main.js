b248b1380dd35eb0a9a724891b6f8df645c882e9
Igor Klopov <igor@klopov.com>
2016-09-22 14:34:34 +0300

    tests for baking v8 options

---
+++ test/test-50-bakery-3/main.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const path = require('path');
+const assert = require('assert');
+const utils = require('../utils.js');
+
+assert(!module.parent);
+assert(__dirname === process.cwd());
+
+const target = process.argv[2] || 'latest';
+const input = './test-x-index.js';
+const output = './run-time/test-output.exe';
+
+let left, right;
+utils.mkdirp.sync(path.dirname(output));
+
+left = utils.spawn.sync(
+  'node', [ '--expose-gc', path.basename(input) ],
+  { cwd: path.dirname(input) }
+);
+
+utils.pkg.sync([
+  '--target', target,
+  '--options', 'expose-gc',
+  '--output', output, input
+]);
+
+right = utils.spawn.sync(
+  './' + path.basename(output), [],
+  { cwd: path.dirname(output) }
+);
+
+assert.equal(left, 'function\n');
+assert.equal(right, 'function\n');
+utils.vacuum.sync(path.dirname(output));
