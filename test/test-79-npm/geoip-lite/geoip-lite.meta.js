31124597029218a916c54b8d152185ca4864ae0b
igorklopov <igor@klopov.com>
2016-10-03 13:06:06 +0300

    seems geoip-lite is ok in travis and in droplets. now arm is the aim

--- test/test-79-npm/geoip-lite/geoip-lite.meta.js
+++ test/test-79-npm/geoip-lite/geoip-lite.meta.js
@@ -2,6 +2,6 @@
 
 module.exports = function (stamp) {
   return {
-    allow: (!(/^arm/).test(stamp.a)) // TODO fabricator failed with SIGKILL. maybe 500 mb mem droplet is too weak?
+    allow: (!(/^arm/).test(stamp.a)) // TODO memory consumption aim is to compile geoip-lite on arm
   };
 };
