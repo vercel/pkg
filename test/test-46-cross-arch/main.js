88b91aa531bfcb6eb19a2a74bbc7faf8c49c2382
igorklopov <igor@klopov.com>
2016-10-03 19:24:42 +0300

    new test: cross-arch linux-x64 linux-armv7

---
+++ test/test-46-cross-arch/main.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const assert = require('assert');
+const utils = require('../utils.js');
+
+assert(!module.parent);
+assert(__dirname === process.cwd());
+
+const opposite = { x64: 'armv7',
+  x86: 'armv7', ia32: 'armv7', arm: 'x64' };
+
+const target = opposite[process.arch];
+const input = './test-x-index.js';
+const output = './test-output.exe';
+
+// only linux-x64 has linux-armv7 counterpart
+if (process.platform !== 'linux') return;
+
+let right;
+
+right = utils.pkg.sync([
+  '--target', target,
+  '--output', output, input
+], { stdio: 'pipe', expect: 2 });
+
+assert(right.stdout.indexOf('\x1B\x5B') < 0, 'colors detected');
+assert(right.stdout.indexOf('Error!') >= 0);
+assert(right.stdout.indexOf(target) >= 0);
+utils.vacuum.sync(output);
