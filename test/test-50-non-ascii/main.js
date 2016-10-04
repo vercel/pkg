1f36a177e77f97be3b2d1ce83b22ed2859df0872
igorklopov <igor@klopov.com>
2016-10-04 17:29:27 +0300

    unusual case with non-ascii path

---
+++ test/test-50-non-ascii/main.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const path = require('path');
+const assert = require('assert');
+const utils = require('../utils.js');
+
+assert(!module.parent);
+assert(__dirname === process.cwd());
+
+const target = process.argv[2] || 'host';
+const input = './test-x-index.js';
+const output = './АБРАКАДАБРА/test-output.exe'; // cyrillic
+
+let right;
+utils.mkdirp.sync(path.dirname(output));
+
+utils.pkg.sync([
+  '--target', target,
+  '--output', output, input
+]);
+
+right = utils.spawn.sync(
+  output, [], {}
+);
+
+assert.equal(right, '42\n');
+utils.vacuum.sync(path.dirname(output));
