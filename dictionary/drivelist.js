cb380d2650a6ed6c3e49f92a2173d3a51c9104f1
Igor Klopov <igor@klopov.com>
2017-01-23 09:35:34 +0300

    fix for drivelist@4.0.0

--- dictionary/drivelist.js
+++ dictionary/drivelist.js
@@ -9,6 +9,10 @@ module.exports = {
       'build/scripts.js': [
         'path.join(__dirname, \'..\', \'scripts\')',
         'path.join(path.dirname(process.execPath), \'scripts\')'
+      ],
+      'lib/scripts.js': [
+        'path.join(__dirname, \'..\', \'scripts\')',
+        'path.join(path.dirname(process.execPath), \'scripts\')' // for 4.0.0
       ]
     }
   }
