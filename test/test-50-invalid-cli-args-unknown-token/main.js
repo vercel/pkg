0abfcae2bddde19105eb648c9b8a3b5fd0fa8a46
Igor Klopov <igor@klopov.com>
2017-03-19 21:56:59 +0300

    test for incorrect target tokens detection

---
+++ test/test-50-invalid-cli-args-unknown-token/main.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const assert = require('assert');
+const utils = require('../utils.js');
+
+assert(!module.parent);
+assert(__dirname === process.cwd());
+
+const standard = 'stdout';
+
+let right;
+
+const inspect = (standard === 'stdout')
+  ? [ 'inherit', 'pipe', 'inherit' ]
+  : [ 'inherit', 'inherit', 'pipe' ];
+
+right = utils.pkg.sync([
+  '--debug',
+  '--target', 'node7-x6',
+  '--output', 'no-output', 'test-x-index.js'
+], { stdio: inspect, expect: 2 });
+
+assert(right.indexOf('\x1B\x5B') < 0, 'colors detected');
+assert(right.indexOf('Error!') >= 0);
+assert(right.indexOf('node7-x6') >= 0);
+assert(right.indexOf('Unknown token') >= 0);
