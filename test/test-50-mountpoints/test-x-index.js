970dac8f3b822a8c798e9b5f21984b9aa3337608
Igor Klopov <igor@klopov.com>
2017-04-22 14:27:06 -0700

    fix test-50-mountpoints: argv[1] -> execPath

--- test/test-50-mountpoints/test-x-index.js
+++ test/test-50-mountpoints/test-x-index.js
@@ -7,9 +7,9 @@ var path = require('path');
 
 require('./plugins-C-int/test-y-require-C.js');
 
-// require(path.join(path.dirname(process.argv[1]), "plugins-D-ext/test-y-require-D.js"));
+// require(path.join(path.dirname(process.execPath), "plugins-D-ext/test-y-require-D.js"));
 
-var myDirectory = path.dirname(process.argv[1]);
+var myDirectory = path.dirname(process.execPath);
 
 process.pkg.mount(
   path.join(__dirname, 'plugins-D-ext'),
