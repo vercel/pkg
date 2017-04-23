c1591636e67c9944b019a9e7c545d057221a2e86
Igor Klopov <igor@klopov.com>
2017-04-22 19:32:41 -0700

    no need to patch argv[1] instead of __dirname

--- dictionary/npm.js
+++ dictionary/npm.js
@@ -4,12 +4,6 @@ module.exports = {
   pkg: {
     scripts: [
       'lib/*.js'
-    ],
-    patches: {
-      'lib/help.js': [
-        'path.dirname(__dirname)',
-        'path.dirname(process.argv[1])'
-      ]
-    }
+    ]
   }
 };
