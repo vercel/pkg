38f59ccba2fdbaa04f2566aec39bc796ed4d59fd
Igor Klopov <igor@klopov.com>
2017-04-27 12:36:12 -0700

    test-nonexistent-pj-bin

---
+++ test/test-50-invalid-cli-args-nonexistent-pj-bin/main.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const assert = require('assert');
+const utils = require('../utils.js');
+
+assert(!module.parent);
+assert(__dirname === process.cwd());
+
+const target = process.argv[2] || 'host';
+const standard = 'stdout';
+
+let right;
+
+const inspect = (standard === 'stdout')
+  ? [ 'inherit', 'pipe', 'inherit' ]
+  : [ 'inherit', 'inherit', 'pipe' ];
+
+right = utils.pkg.sync([
+  '--target', target,
+  '--output', 'no-output', '.'
+], { stdio: inspect, expect: 2 });
+
+assert(right.indexOf('\x1B\x5B') < 0, 'colors detected');
+assert(right.indexOf('Error!') >= 0);
+assert(right.indexOf('Property \'bin\' does not exist') >= 0);
+assert(right.indexOf('package.json') >= 0);
