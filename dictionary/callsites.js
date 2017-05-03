d94d3ad5a8424558c5ce2f911665f8ea3e861b39
Igor Klopov <igor@klopov.com>
2017-05-03 13:22:26 -0700

    temporary fix for #58

--- dictionary/callsites.js
+++ dictionary/callsites.js
@@ -1,15 +1,4 @@
 'use strict';
 
 module.exports = {
-  pkg: {
-    patches: {
-      'index.js': [
-        { do: 'erase' },
-        'module.exports = function() {' +
-          'throw new Error("Pkg: \'callsites\' package is ' +
-          'temporarily unsupported. Please create a github issue.")' +
-        '};'
-      ]
-    }
-  }
 };
--- test/test-79-npm/callsites/callsites.js
+++ test/test-79-npm/callsites/callsites.js
@@ -1,17 +1,6 @@
 'use strict';
 
-// only pkg-ed run
-if (!process.pkg) {
-  console.log('ok');
-  return;
-}
-
 var callsites = require('callsites');
-
-try {
-  callsites();
-} catch (error) {
-  if (error.message.indexOf('Pkg') >= 0) {
-    console.log('ok');
-  }
+if (callsites().length >= 4) {
+  console.log('ok');
 }
