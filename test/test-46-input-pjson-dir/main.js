565167850f8afd4ab90dc0092365a615fa385c2b
Igor Klopov <igor@klopov.com>
2017-05-02 21:57:28 -0700

    use name from config (prepare for #54)

---
+++ test/test-46-input-pjson-dir/main.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const assert = require('assert');
+const utils = require('../utils.js');
+
+assert(!module.parent);
+assert(__dirname === process.cwd());
+
+const input = '.';
+
+const newcomers = [
+  'palookaville-linux',
+  'palookaville-macos',
+  'palookaville-win.exe'
+];
+
+const before = utils.filesBefore(newcomers);
+
+utils.pkg.sync([
+  input
+], { stdio: 'inherit' });
+
+utils.filesAfter(before, newcomers);
