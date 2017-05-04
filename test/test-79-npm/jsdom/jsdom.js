d87bcc5704e2ae35f20d276a24310a5cfad7ebb8
Igor Klopov <igor@klopov.com>
2017-05-03 18:07:34 -0700

    fix 'jsdom'

--- test/test-79-npm/jsdom/jsdom.js
+++ test/test-79-npm/jsdom/jsdom.js
@@ -1,12 +1,8 @@
 'use strict';
 
-var jsdom = require('jsdom');
-
-jsdom.env(
-  '<p><a class="the-link" href="https://github.com/tmpvar/jsdom">jsdom!</a></p>',
-  function (errors, window) {
-    if (window.document.body.children[0].tagName === 'P') {
-      console.log('ok');
-    }
-  }
-);
+var JSDOM = require('jsdom').JSDOM;
+var dom = new JSDOM('<p><a class="the-link" href="https://github.com/tmpvar/jsdom">jsdom!</a></p>');
+var window = dom.window;
+if (window.document.body.children[0].tagName === 'P') {
+  console.log('ok');
+}
