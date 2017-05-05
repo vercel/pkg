103b06b9f2f44c5b453fb7b2cbc8ff63421c3900
Igor Klopov <igor@klopov.com>
2017-05-04 17:19:08 -0700

    fix node-notifier test for headless linux

--- test/test-79-npm/node-notifier/node-notifier.js
+++ test/test-79-npm/node-notifier/node-notifier.js
@@ -2,10 +2,14 @@
 
 var notifier = require('node-notifier');
 var utils = require('node-notifier/lib/utils.js');
-utils.fileCommandJson = function (filename) {
+utils.command = utils.fileCommandJson = function (filename) {
   if (!(/^\/snapshot\//.test(filename))) {
     console.log('ok');
   }
   process.exit();
+};
+var which = require('which');
+which.sync = function () {
+  return true;
 };
 notifier.notify('hi!');
