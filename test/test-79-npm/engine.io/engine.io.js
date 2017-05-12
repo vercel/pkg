1bcdc720fdd2426f959404e2c7961eac06c02d8e
Igor Klopov <igor@klopov.com>
2017-05-13 02:27:17 +0300

    dictionary: engine.io. fixes #86

---
+++ test/test-79-npm/engine.io/engine.io.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var http = require('http');
+var engine = require('engine.io');
+engine.listen(3000);
+
+setTimeout(function () {
+  http.get('http://127.0.0.1:3000/socket.io/socket.io.js', function (res) {
+    var chunks = '';
+    res.on('data', function (chunk) {
+      chunks += chunk.toString();
+    }).on('end', function () {
+      if (chunks === 'Not Implemented') {
+        console.log('ok');
+      }
+      process.exit();
+    });
+  });
+}, 100);
