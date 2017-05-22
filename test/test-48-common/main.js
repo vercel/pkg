f62d18e4a765f5b2ce79b85cf1c1a4346bcfbdcf
Igor Klopov <igor@klopov.com>
2017-05-22 12:02:32 +0300

    test-48-common to cover more cases

--- test/test-48-common/main.js
+++ test/test-48-common/main.js
@@ -16,7 +16,7 @@ if (process.platform === 'win32') {
   assert.equal('C:\\snapshot',           common.normalizePath('c:\\snapshot\\'));
   assert.equal('C:\\snapshoter',         common.normalizePath('c:\\snapshoter\\'));
   assert.equal('C:\\snapshot\\foo',      common.normalizePath('c:\\snapshot\\\\foo'));
-  assert.equal('C:\\snapshot\\foo\\bar', common.normalizePath('c:\\snapshot\\\\foo\\\\bar\\\\\\'));
+  assert.equal('C:\\snapshot\\foo\\bar', common.normalizePath('c:\\snapshot\\\\foo\\\\bar\\/\\\\'));
 
   assert.equal(common.insideSnapshot('c:'), false);
   assert.equal(common.insideSnapshot('c:\\'), false);
@@ -37,14 +37,14 @@ if (process.platform === 'win32') {
   assert.equal('C:\\',             common.stripSnapshot('c:\\snapshot\\'));
   assert.equal('c:\\snapshoter\\', common.stripSnapshot('c:\\snapshoter\\'));
   assert.equal('C:\\foo',          common.stripSnapshot('c:\\snapshot\\\\foo'));
-  assert.equal('C:\\foo\\bar',     common.stripSnapshot('c:\\snapshot\\\\foo\\\\bar\\\\\\'));
+  assert.equal('C:\\foo\\bar',     common.stripSnapshot('c:\\snapshot\\\\foo\\\\bar\\/\\\\'));
 
   assert.equal('C:\\snapshot',           common.snapshotify('c:\\'));
   assert.equal('C:\\snapshot\\foo',      common.snapshotify('c:\\foo'));
   assert.equal('C:\\snapshot\\foo',      common.snapshotify('c:\\foo\\'));
   assert.equal('C:\\snapshot\\foo',      common.snapshotify('c:\\foo\\\\'));
   assert.equal('C:\\snapshot\\foo\\bar', common.snapshotify('c:\\foo\\\\bar'));
-  assert.equal('C:\\snapshot\\foo\\bar', common.snapshotify('c:\\foo\\\\bar\\\\\\'));
+  assert.equal('C:\\snapshot\\foo\\bar', common.snapshotify('c:\\foo\\\\bar\\/\\\\'));
 } else {
   assert.equal('/',                 common.normalizePath('/'));
   assert.equal('/',                 common.normalizePath('//'));
@@ -53,7 +53,7 @@ if (process.platform === 'win32') {
   assert.equal('/snapshot',         common.normalizePath('/snapshot/'));
   assert.equal('/snapshoter',       common.normalizePath('/snapshoter/'));
   assert.equal('/snapshot/foo',     common.normalizePath('/snapshot//foo'));
-  assert.equal('/snapshot/foo/bar', common.normalizePath('/snapshot//foo//bar///'));
+  assert.equal('/snapshot/foo/bar', common.normalizePath('/snapshot//foo//bar/\\//'));
 
   assert.equal(common.insideSnapshot(''), false);
   assert.equal(common.insideSnapshot('/'), false);
@@ -74,12 +74,12 @@ if (process.platform === 'win32') {
   assert.equal('/',            common.stripSnapshot('/snapshot/'));
   assert.equal('/snapshoter/', common.stripSnapshot('/snapshoter/'));
   assert.equal('/foo',         common.stripSnapshot('/snapshot//foo'));
-  assert.equal('/foo/bar',     common.stripSnapshot('/snapshot//foo//bar///'));
+  assert.equal('/foo/bar',     common.stripSnapshot('/snapshot//foo//bar/\\//'));
 
   assert.equal('/snapshot',         common.snapshotify('/'));
   assert.equal('/snapshot/foo',     common.snapshotify('/foo'));
   assert.equal('/snapshot/foo',     common.snapshotify('/foo/'));
   assert.equal('/snapshot/foo',     common.snapshotify('/foo//'));
   assert.equal('/snapshot/foo/bar', common.snapshotify('/foo//bar'));
-  assert.equal('/snapshot/foo/bar', common.snapshotify('/foo//bar///'));
+  assert.equal('/snapshot/foo/bar', common.snapshotify('/foo//bar/\\//'));
 }
