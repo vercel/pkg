ebcbcfbcc5e8dcc38dba1b0d780e02e156ec0de9
Igor Klopov <igor@klopov.com>
2017-05-29 23:11:50 +0300

    no more 'module_root' hack for 'bindings'

--- dictionary/contextify.js
+++ dictionary/contextify.js
@@ -1,12 +1,4 @@
 'use strict';
 
 module.exports = {
-  pkg: {
-    patches: {
-      'lib/contextify.js': [
-        'require(\'bindings\')(\'contextify\')',
-        'require(\'bindings\')({ bindings: \'contextify\', module_root: __dirname + \'/..\' })'
-      ]
-    }
-  }
 };
--- dictionary/curve25519.js
+++ dictionary/curve25519.js
@@ -1,12 +1,4 @@
 'use strict';
 
 module.exports = {
-  pkg: {
-    patches: {
-      'index.js': [
-        'require("bindings")("curve")',
-        'require("bindings")({ bindings: "curve", module_root: __dirname })'
-      ]
-    }
-  }
 };
