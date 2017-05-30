9b50d1b9c2a1c319ac762b62fbf7ed0cbefeb72f
Igor Klopov <igor@klopov.com>
2017-05-30 21:47:45 +0300

    minor fixes 3

---
+++ test/test-79-npm/nodegit/nodegit.meta.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const home = require('../home.js');
+
+module.exports = function (stamp) {
+  return {
+    allow: home(stamp) && (stamp.m >= 51),
+    note: 'precompiled binary is for node7 only'
+  };
+};
