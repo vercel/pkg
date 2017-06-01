d6a44b01338119b186e78ec1d991e3fa238bceec
Igor Klopov <igor@klopov.com>
2017-06-01 11:58:47 +0300

    fix test for update-notifier

--- test/test-79-npm/update-notifier/update-notifier.js
+++ test/test-79-npm/update-notifier/update-notifier.js
@@ -1,12 +1,37 @@
 'use strict';
 
+var spawnWasCalled;
+var save = require('child_process').spawn;
+require('child_process').spawn = function () {
+  spawnWasCalled = true;
+  return save.apply(this, arguments); // eslint-disable-line prefer-rest-params
+};
+
 var updateNotifier = require('update-notifier');
 var pjson = { name: 'pkg', version: '3.0.0' };
-var notifier = updateNotifier({ pkg: pjson, updateCheckInterval: 1000 });
+var notifier;
 
-setInterval(function () {
-  if (notifier.update && notifier.update.current === '3.0.0') {
-    console.log('ok');
+function runNotifier (cb) {
+  notifier = updateNotifier({
+    pkg: pjson,
+    updateCheckInterval: 0
+  });
+  setTimeout(function () {
+    cb(notifier.update);
+  }, 950);
+}
+
+// never works first time.
+// it seems to create some
+// persistent structures
+
+runNotifier(function () {
+  runNotifier(function (update) {
+    if (spawnWasCalled &&
+        update &&
+        update.current === '3.0.0') {
+      console.log('ok');
+    }
     process.exit(0);
-  }
-}, 450);
+  });
+});
