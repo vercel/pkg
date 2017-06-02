7d476fd971908ca9396f886e89b701f456e066f1
Igor Klopov <igor@klopov.com>
2017-06-03 00:53:46 +0300

    fix complexity + test/spawn-c for win32

--- test/test-50-fs-runtime-layer/test-x2-index.js
+++ test/test-50-fs-runtime-layer/test-x2-index.js
@@ -1,3 +1,4 @@
+/* eslint-disable complexity */
 /* eslint-disable indent */
 /* eslint-disable no-path-concat */
 
--- test/test-50-spawn/test-spawn-c-child.js
+++ test/test-50-spawn/test-spawn-c-child.js
@@ -8,5 +8,6 @@ var cluster = require('cluster');
 assert(!process.send);
 assert(!cluster.worker);
 
-console.log('Hello from execFileSync-child!');
+console.log('Hello from spawn-c-child!');
 console.log('Args', JSON.stringify(process.argv.slice(2)));
+process.exit(4 + process.argv.length);
