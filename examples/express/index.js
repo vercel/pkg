d569dccc8a63f260aec8fa255ea209303f68fc0c
Igor Klopov <igor@klopov.com>
2017-06-05 16:24:56 +0300

    fix express example

--- examples/express/index.js
+++ examples/express/index.js
@@ -9,7 +9,7 @@ server.listen(8080);
 // __dirname is used here along with package.json.pkg.assets
 // see https://github.com/zeit/pkg#config and
 // https://github.com/zeit/pkg#snapshot-filesystem
-app.use('/lib', express.static(__dirname + '/views/lib'));
+app.use('/', express.static(__dirname + '/views'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
