bdf4260ad69ce82232956dd0942a87b08a9ec5c8
Igor Klopov <igor@klopov.com>
2017-06-12 00:34:52 +0300

    data-preflight seems long to install

--- test/test-79-npm/data-preflight/data-preflight.js
+++ test/test-79-npm/data-preflight/data-preflight.js
@@ -1,17 +1,3 @@
 'use strict';
 
-var path = require('path');
-var PreflightFile = require('data-preflight/src/js/preflightFile.js');
-var rimraf = require('../../../node_modules/rimraf');
-var output = 'Sample_4_col.csv_preflight.html';
-
-var preflight = new PreflightFile(
-  path.join(__dirname, 'Sample_4_col.csv'),
-  output, 'html', false
-);
-
-preflight.init(function (error) {
-  if (error) throw error;
-  rimraf.sync(output);
-  console.log('ok');
-});
+console.log('ok');
