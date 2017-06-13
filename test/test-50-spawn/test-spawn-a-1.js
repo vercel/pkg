fe09b4cfda11e94bd80279c8ed9ab4625d39522d
Igor Klopov <igor@klopov.com>
2017-06-13 07:53:40 +0300

    Revert "take node0 child.send args into consideration"
    This reverts commit eb0cb99d4c78046db8024d3bb748a9af3759c2ab.

--- test/test-50-spawn/test-spawn-a-1.js
+++ test/test-50-spawn/test-spawn-a-1.js
@@ -14,19 +14,13 @@ var child = spawn(
   { stdio: [ 'inherit', 'inherit', 'inherit', 'ipc' ] }
 );
 
-function ifError (error) {
-  if (error) {
-    console.log('child.send.error', error);
-  }
-}
-
 child.on('message', function (value) {
   console.log(value.toString());
-  if ((process.version.slice(1, 3) | 0) > 0) {
-    child.send(value, ifError);
-  } else {
-    child.send(value);
-  }
+  child.send(value, function (error) {
+    if (error) {
+      console.log('child.send.error', error);
+    }
+  });
 });
 
 child.send(2);
