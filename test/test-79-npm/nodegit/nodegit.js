99fc0060565c2c1e226c1cf03426f6c1bf7b4c27
Igor Klopov <igor@klopov.com>
2017-06-23 18:00:54 +0300

    include nodegit extensions into binary. fixes #120

--- test/test-79-npm/nodegit/nodegit.js
+++ test/test-79-npm/nodegit/nodegit.js
@@ -1,4 +1,12 @@
 'use strict';
 
-require('nodegit');
-console.log('ok');
+var NodeGit = require('nodegit');
+var Diff = NodeGit.Diff;
+
+NodeGit.Repository.init('./', 0).then(function (repo) {
+  return Diff.indexToWorkdir(repo, null, {
+    flags: Diff.OPTION.INCLUDE_UNTRACKED | Diff.OPTION.RECURSE_UNTRACKED_DIRS
+  });
+}).then(function () {
+  console.log('ok');
+});
