82c12b25c083ca07dc7b319f8e4b2d6f4367544c
Raidou <weirongxu.raidou@gmail.com>
2017-06-30 16:47:55 +0800

    dictionary: bunyan

---
+++ dictionary/bunyan.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  pkg: {
+    patches: {
+      'lib/bunyan.js': [
+        'mv = require(\'mv\' + \'\');',
+        'mv = require(\'mv\');'
+      ]
+    }
+  }
+};
