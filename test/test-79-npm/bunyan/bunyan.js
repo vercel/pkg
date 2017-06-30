55827efa0dc5fd542bb679a1552cfe81a6f114d1
Weirong Xu <weirongxu.raidou@gmail.com>
2017-07-01 01:27:30 +0800

    fix test

---
+++ test/test-79-npm/bunyan/bunyan.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var bunyan = require('bunyan');
+var logger = bunyan.createLogger({
+  name: 'pkg',
+  streams: [
+    {
+      type: 'rotating-file',
+      path: 'pkg.log',
+      level: 'info'
+    }
+  ]
+});
+
+if (logger) {
+  console.log('ok');
+}
