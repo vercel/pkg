d3228e24044aacce0133bce99c833d82f3229c32
Igor Klopov <igor@klopov.com>
2017-07-14 19:51:47 +0300

    workaround not needed for bindings since callsites fix

--- dictionary/bindings.js
+++ dictionary/bindings.js
@@ -1,17 +1,4 @@
 'use strict';
 
 module.exports = {
-  pkg: {
-    patches: {
-      // WARNING! THIS IS A FALLBACK!
-      // CREATE A DICTIONARY LIKE LEVELDOWN AND CONTEXTIFY
-      // USE 'module_root' OPTION TO BINDING
-      // THIS IS BECAUSE NODE 5+ (NPM 3+) HAS FLAT NODE_MODULES
-      // AND NATIVE ADDON IS NOT 2 LEVELS UP ANYMORE, BUT NEARBY
-      'bindings.js': [
-        'return fileName',
-        'return dirname(__dirname)' // <- 2 levels up
-      ]
-    }
-  }
 };
