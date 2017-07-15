fb9d097da7c29cce1fba9ab55fab1d48dcdebfc5
Igor Klopov <igor@klopov.com>
2017-07-15 12:42:04 +0300

    test-50-many-addons. new approch to testing native addons

--- test/test-50-many-addons/main.js
+++ test/test-50-many-addons/main.js
@@ -12,17 +12,8 @@ assert(__dirname === process.cwd());
 
 const host = 'node' + process.version[1];
 const target = process.argv[2] || host;
-const windows = process.platform === 'win32';
 const input = './test-x-index.js';
 const output = './run-time/test-output.exe';
-
-if (!windows) return;
-const arch = process.arch; // TODO extract arch from `target` once it contains
-if (arch !== 'x64') return;
-const version1 = process.version;
-if (!(/^(node|v)?4/.test(version1))) return;
-const version2 = target;
-if (!(/^(node|v)?4/.test(version2))) return;
 
 let left, right;
 utils.mkdirp.sync(path.dirname(output));
--- test/test-50-many-addons/test-x-index.js
+++ test/test-50-many-addons/test-x-index.js
@@ -1,4 +1,9 @@
+/* eslint-disable no-underscore-dangle */
+
 'use strict';
 
+const Module = require('module');
+Module._extensions['.node'] = Module._extensions['.js'];
+console.log(typeof require('./time.node').time);
 console.log(typeof require('./sub1/time.node').time);
 console.log(typeof require('./sub1/sub2/time.node').time);
