a393ec7f7ab411bd93dfc667e41a5a3be2bcebea
Igor Klopov <igor@klopov.com>
2017-07-15 18:23:37 +0300

    fix log4js test

--- test/test-79-npm/log4js/log4js.js
+++ test/test-79-npm/log4js/log4js.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var log4js = require('log4js');
-log4js.loadAppender('file');
 var logger = log4js.getLogger('cheese');
-logger.setLevel('ERROR');
-console.log('ok');
+if (logger.log) {
+  console.log('ok');
+}
--- test/test-79-npm/log4js/log4js@0.5.8.js
+++ test/test-79-npm/log4js/log4js@0.5.8.js
@@ -1,3 +1,7 @@
 'use strict';
 
-require('./log4js.js');
+var log4js = require('log4js');
+log4js.loadAppender('file');
+var logger = log4js.getLogger('cheese');
+logger.setLevel('ERROR');
+console.log('ok');
