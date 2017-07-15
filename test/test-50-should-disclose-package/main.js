c5f191bb4e8fddcbb26791aaf703c76b9a50505f
Igor Klopov <igor@klopov.com>
2017-07-15 17:56:35 +0300

    --public option to disclose the sources of top-level project

--- test/test-50-should-disclose-package/main.js
+++ test/test-50-should-disclose-package/main.js
@@ -14,24 +14,7 @@ const input = './test-x-index.js';
 const output = './test-output.exe';
 const standard = 'stdout';
 
-let right;
-
-const inspect = (standard === 'stdout')
-  ? [ 'inherit', 'pipe', 'inherit' ]
-  : [ 'inherit', 'inherit', 'pipe' ];
-
-right = utils.pkg.sync([
-  '--debug',
-  '--target', target,
-  '--output', output, input
-], inspect);
-
-assert(right.indexOf('\x1B\x5B') < 0, 'colors detected');
-
-const mappy = {};
-
-right = right.split('\n');
-right.some(function (line, index) {
+function rightReducer (mappy, line, index, right) {
   if ((line.indexOf('Cannot resolve') >= 0) ||
       (line.indexOf('The file was included') >= 0)) {
     let name = path.basename(right[index + 1]);
@@ -41,22 +24,44 @@ right.some(function (line, index) {
     if (mappy[name]) assert.equal(mappy[name], value);
     mappy[name] = value;
   }
-});
 
-const lines = Object.keys(mappy).sort().map(function (key) {
-  return key + ' = ' + mappy[key];
-}).join('\n') + '\n';
+  return mappy;
+}
 
-assert.equal(lines,
-  'connect.js = DISCLOSED code (with sources)\n' +
-  'has-no-license.js = bytecode (no sources)\n' +
-  'has-no-license.js (w) = Cannot resolve \'hasNoLicenseNonLiteral\'\n' +
-  'has-permissive-license.js = DISCLOSED code (with sources)\n' +
-  'has-permissive-license.js (w) = Cannot resolve \'hasPermissiveLicenseNonLiteral\'\n' +
-  'has-strict-license.js = bytecode (no sources)\n' +
-  'has-strict-license.js (w) = Cannot resolve \'hasStrictLicenseNonLiteral\'\n' +
-  'package.json = DISCLOSED code (with sources)\n' +
-  'test-x-index.js = bytecode (no sources)\n'
-);
+for (const pub of [ false, true ]) {
+  let right;
 
-utils.vacuum.sync(output);
+  const inspect = (standard === 'stdout')
+    ? [ 'inherit', 'pipe', 'inherit' ]
+    : [ 'inherit', 'inherit', 'pipe' ];
+
+  right = utils.pkg.sync([
+    '--debug',
+    '--target', target,
+    '--output', output, input
+  ].concat(pub ? [ '--public' ] : []), inspect);
+
+  assert(right.indexOf('\x1B\x5B') < 0, 'colors detected');
+
+  right = right.split('\n');
+  const mappy = right.reduce(rightReducer, {});
+
+  const lines = Object.keys(mappy).sort().map(function (key) {
+    return key + ' = ' + mappy[key];
+  }).join('\n') + '\n';
+
+  assert.equal(lines,
+    'connect.js = DISCLOSED code (with sources)\n' +
+    'has-no-license.js = bytecode (no sources)\n' +
+    'has-no-license.js (w) = Cannot resolve \'hasNoLicenseNonLiteral\'\n' +
+    'has-permissive-license.js = DISCLOSED code (with sources)\n' +
+    'has-permissive-license.js (w) = Cannot resolve \'hasPermissiveLicenseNonLiteral\'\n' +
+    'has-strict-license.js = bytecode (no sources)\n' +
+    'has-strict-license.js (w) = Cannot resolve \'hasStrictLicenseNonLiteral\'\n' +
+    'package.json = DISCLOSED code (with sources)\n' +
+    (pub ? 'test-x-index.js = DISCLOSED code (with sources)\n' :
+           'test-x-index.js = bytecode (no sources)\n')
+  );
+
+  utils.vacuum.sync(output);
+}
