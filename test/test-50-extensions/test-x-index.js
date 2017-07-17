776f298e4573ebf77ab4bb186b0da194f85079e6
Igor Klopov <igor@klopov.com>
2017-07-17 20:24:56 +0300

    proper logging and a test. closer to #123

---
+++ test/test-50-extensions/test-x-index.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-underscore-dangle */
+
+var Module = require('module');
+var fs = require('fs');
+
+Module._extensions['.js'] = function (module, filename) {
+  var content = fs.readFileSync(filename, 'utf8');
+  // emulating babel-register
+  content = content.replace(
+    'import x from \'./test-z-sub.js\';',
+    'require(\'./test-z-sub.js\');');
+  module._compile(content, filename);
+};
+
+require('./test-y-esnext.js');
+require.resolve('./test-z-sub.js');
---
+++ test/test-50-extensions/test-y-esnext.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-unused-vars */
+
+import x from './test-z-sub.js';
