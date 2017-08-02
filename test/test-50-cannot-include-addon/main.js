1c26127178fa1313cb00c531405fe714398d4c90
Igor Klopov <igor@klopov.com>
2017-08-02 12:12:16 +0300

    test to check 'native addon' warning

---
+++ test/test-50-cannot-include-addon/main.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const assert = require('assert');
+const utils = require('../utils.js');
+
+assert(!module.parent);
+assert(__dirname === process.cwd());
+
+const target = process.argv[2] || 'host';
+const input = './test-x-index.js';
+const output = './test-output.exe';
+const standard = 'stdout';
+
+let right;
+
+const inspect = (standard === 'stdout')
+  ? [ 'inherit', 'pipe', 'inherit' ]
+  : [ 'inherit', 'inherit', 'pipe' ];
+
+right = utils.pkg.sync([
+  '--target', target,
+  '--output', output, input
+], inspect);
+
+assert(right.indexOf('\x1B\x5B') < 0, 'colors detected');
+assert(right.indexOf('test-50-cannot-include-addon/time.node') >= 0);
+assert(right.indexOf('path-to-executable/time.node') >= 0);
+utils.vacuum.sync(output);
