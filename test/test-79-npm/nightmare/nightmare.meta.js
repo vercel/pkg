49db7fa0fbbc4163c85c2ba9fc623572a30506b8
Igor Klopov <igor@klopov.com>
2017-08-03 17:15:32 +0300

    revoke nightmare from all ci platforms

--- test/test-79-npm/nightmare/nightmare.meta.js
+++ test/test-79-npm/nightmare/nightmare.meta.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = function (stamp, flags) {
-  if (stamp.p === 'win32' && flags.ci) {
+  if (flags.ci) {
     return {
       allow: false,
-      note: 'windows CI seems to hang'
+      note: 'headless CI seems to fail headful electron'
     };
   }
 
