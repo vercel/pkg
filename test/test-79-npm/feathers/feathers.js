e6aefc7c7e7dd6d96deeebfa37f4e4a55b8cfa89
Igor Klopov <igor@klopov.com>
2017-08-19 18:13:56 +0300

    test for feathers

---
+++ test/test-79-npm/feathers/feathers.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const feathers = require('feathers');
+const http = require('http');
+const app = feathers();
+
+app.get('/', function (req, res) {
+  res.end('Hello there!');
+});
+
+var server = app.listen(1337, function () {
+  var port = server.address().port;
+  setTimeout(function () {
+    http.get('http://127.0.0.1:' + port.toString() + '/', function (res) {
+      var chunks = '';
+      res.on('data', function (chunk) {
+        chunks += chunk.toString();
+      }).on('end', function () {
+        if (chunks === 'Hello there!') {
+          console.log('ok');
+        }
+        server.close();
+      });
+    });
+  }, 100);
+});
