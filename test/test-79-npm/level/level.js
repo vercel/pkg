f579a2d75fd97531cbd5aa1eb24e6a81775c32b8
Igor Klopov <igor@klopov.com>
2017-08-25 18:30:11 +0300

    test for level npm package

---
+++ test/test-79-npm/level/level.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var level = require('level');
+var db = level('dbname');
+var rimraf = require('../../../node_modules/rimraf');
+
+(function (cb) {
+  db.open(function (error1) {
+    if (error1) return cb(error1);
+
+    db.put('dave@gmail.com', JSON.stringify({
+      _id: 'dave@gmail.com',
+      name: 'David',
+      age: 68
+    }), function (error2) {
+      if (error2) return cb(error2);
+      db.get('dave@gmail.com', function (error3, data) {
+        if (error3) return cb(error3);
+        data = JSON.parse(data.toString());
+        if (data.age === 68) {
+          console.log('ok');
+          return cb();
+        }
+      });
+    });
+  });
+}(function (error) {
+  if (error) throw error;
+  db.close(function () {
+    rimraf.sync('dbname');
+  });
+}));
