4a2055e94718e2d30d5cb5e4cadedd7b9adea764
Igor Klopov <igor@klopov.com>
2017-09-07 23:51:25 +0300

    skip opn test in appveyor

--- test/test-79-npm/opn/opn.meta.js
+++ test/test-79-npm/opn/opn.meta.js
@@ -1,6 +1,13 @@
 'use strict';
 
-module.exports = function () {
+module.exports = function (stamp, flags) {
+  if (stamp.p === 'win32' && flags.ci) {
+    return {
+      allow: false,
+      note: 'headless windows CI seems to fail'
+    };
+  }
+
   return {
     deployFilesFrom: [ 'opn' ]
   };
