939c0dfb700fd709b05222e19dcdfe14a19d8f26
Igor Klopov <igor@klopov.com>
2017-09-15 23:48:51 +0200

    chdir to proc.env.PWD, that can be passed explicitly. fixes #231

---
+++ test/test-50-chdir-env-var/main.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const path = require('path');
+const assert = require('assert');
+const utils = require('../utils.js');
+
+assert(!module.parent);
+assert(__dirname === process.cwd());
+
+const target = process.argv[2] || 'host';
+const input = './test-x-index.js';
+const output = './run-time/test-output.exe';
+
+let right;
+
+utils.pkg.sync([
+  '--target', target,
+  '--output', output, input
+], { env: {
+  CHDIR: 'source',
+  PATH: process.env.PATH
+} });
+
+right = utils.spawn.sync(
+  './' + path.basename(output), [],
+  { cwd: path.dirname('source/' + output) }
+);
+
+assert.equal(right, 'ok\n');
+utils.vacuum.sync(path.dirname('source/' + output));
