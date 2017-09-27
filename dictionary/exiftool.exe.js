28c32a995ba5116a86d9af1fe5f5290a6be76b9c
Mika Fischer <mika.fischer@zoopnet.de>
2017-09-27 14:56:52 +0200

    Support exiftool.exe and exiftool.pl for dist-exiftool

---
+++ dictionary/exiftool.exe.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  pkg: {
+    patches: {
+      'index.js': [
+        'path.join(__dirname, \'vendor\', \'exiftool.exe\')',
+        'path.join(path.dirname(process.execPath), \'exiftool.exe\')'
+      ]
+    },
+    deployFiles: [
+      [ 'vendor/exiftool.exe', 'exiftool.exe' ]
+    ]
+  }
+};
+
---
+++ dictionary/exiftool.pl.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  pkg: {
+    patches: {
+      'index.js': [
+        'path.join(__dirname, \'vendor\', \'exiftool\')',
+        'path.join(path.dirname(process.execPath), \'exiftool\')'
+      ]
+    },
+    deployFiles: [
+      [ 'vendor/exiftool', 'exiftool' ]
+    ]
+  }
+};
+
