e7d0c1d46e3a66a36ca464c94af78c90811a9900
Igor Klopov <igor@klopov.com>
2017-11-01 20:58:09 +0300

    test for arguments. fails currently

---
+++ test/test-50-arguments/main.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const path = require('path');
+const assert = require('assert');
+const utils = require('../utils.js');
+
+assert(!module.parent);
+assert(__dirname === process.cwd());
+
+const target = process.argv[2] || 'host';
+const input = './test-x-index.js';
+const output = './test-output.exe';
+
+let right;
+utils.mkdirp.sync(path.dirname(output));
+
+utils.pkg.sync([
+  '--target', target,
+  '--output', output, input
+]);
+
+right = utils.spawn.sync(output, [ '42' ], {});
+assert.equal(right, '42\n');
+right = utils.spawn.sync(output, [ '-ft' ], {});
+assert.equal(right, '-ft\n');
+right = utils.spawn.sync(output, [ '--fourty-two' ], {});
+assert.equal(right, '--fourty-two\n');
+utils.vacuum.sync(path.dirname(output));
