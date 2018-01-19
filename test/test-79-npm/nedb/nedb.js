bff58f15d699b786a32fdf269ee00682354f5a2e
Igor Klopov <igor@klopov.com>
2018-01-19 19:52:49 +0300

    proper cleanup in nedb test

--- test/test-79-npm/nedb/nedb.js
+++ test/test-79-npm/nedb/nedb.js
@@ -1,12 +1,15 @@
 'use strict';
 
 var Nedb = require('nedb');
-var db = new Nedb({ filename: 'database.db' });
+var fs = require('fs');
+var filename = 'database.db';
+var db = new Nedb({ filename: filename });
 
 db.loadDatabase(function (error) {
   if (error) throw error;
   db.insert([ { a: 5 }, { a: 42 } ], function (error2) {
     if (error2) throw error2;
+    fs.unlinkSync(filename);
     console.log('ok');
   });
 });
