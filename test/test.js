203b0303b2dbb9bbe023ebae68f35b71b22c5698
Igor Klopov <igor@klopov.com>
2018-05-15 16:33:38 -0700

    read major node version properly

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ const chalk = require('chalk');
 const globby = require('globby');
 const path = require('path');
 const utils = require('./utils.js');
-const host = 'node' + process.version[1];
+const host = 'node' + process.version.match(/^v(\d+)/)[1];
 let target = process.argv[2] || 'host';
 if (target === 'host') target = host;
 const modify = process.argv[3] || 'all';
