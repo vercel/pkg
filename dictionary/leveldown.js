1db8310508171c50e389fb119f418a4361d307fe
IgorKlopov <igor@klopov.com>
2020-01-18 23:32:26 +0300

    update leveldown dictionary. addresses #758 and #825

--- dictionary/leveldown.js
+++ dictionary/leveldown.js
@@ -1,4 +1,15 @@
 'use strict';
 
 module.exports = {
+  pkg: {
+    patches: {
+      'binding.js': [
+        '__dirname',
+        'require(\'path\').dirname(process.execPath)'
+      ]
+    },
+    deployFiles: [
+      [ 'prebuilds', 'prebuilds', 'directory' ]
+    ]
+  }
 };
--- test/test-79-npm/leveldown/leveldown.meta.js
+++ test/test-79-npm/leveldown/leveldown.meta.js
@@ -4,6 +4,7 @@ const home = require('../home.js');
 
 module.exports = function (stamp) {
   return {
-    allow: home(stamp)
+    allow: home(stamp),
+    deployFilesFrom: [ 'leveldown' ]
   };
 };
