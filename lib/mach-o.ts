17f8b8a1b25be6b9ec4f172155f624e2e2593b8d
Brian Alonso <brian.alonso.a@gmail.com>
2022-09-14 08:49:45 -0300

    Add force flag to codesign to avoid already signed error

--- lib/mach-o.ts
+++ lib/mach-o.ts
@@ -59,7 +59,7 @@ function patchMachOExecutable(file: Buffer) {
 
 function signMachOExecutable(executable: string) {
   try {
-    execFileSync('codesign', ['--sign', '-', executable], { stdio: 'inherit' });
+    execFileSync('codesign', ['-f', '--sign', '-', executable], { stdio: 'inherit' });
   } catch {
     execFileSync('ldid', ['-Cadhoc', '-S', executable], { stdio: 'inherit' });
   }
