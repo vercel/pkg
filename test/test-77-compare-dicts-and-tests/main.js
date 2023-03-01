13884ba72d837d7ea1750238d6345654eb9b64b2
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-03-01 14:36:47 +0800

    extract inline function

--- test/test-77-compare-dicts-and-tests/main.js
+++ test/test-77-compare-dicts-and-tests/main.js
@@ -8,14 +8,16 @@ const assert = require('assert');
 
 const configs = Object.keys(require('../../lib-es5/dictionary').default);
 
-const tests = fs.readdirSync('../test-79-npm').filter(function (test) {
-  if (test === '_isolator') return false;
-  const full = path.join('../test-79-npm', test);
+const directory = '../test-79-npm';
+function onlyDirectories(name) {
+  if (name === '_isolator') return false;
+  const full = path.join(directory, name);
   return fs.statSync(full).isDirectory();
-});
+}
+const folders = fs.readdirSync(directory).filter(onlyDirectories);
 
-tests.push('etc'); // TODO who creates it?
-tests.push('steam-resources'); // absent in npm. installed via github
+folders.push('etc'); // TODO who creates it?
+folders.push('steam-resources'); // absent in npm. installed via github
 
 configs.push('etc'); // TODO who creates it?
 configs.push('express-with-jade');
@@ -24,13 +26,13 @@ configs.push('redis-with-hiredis');
 let absent = false;
 
 configs.some(function (config) {
-  if (tests.indexOf(config) < 0) {
+  if (folders.indexOf(config) < 0) {
     console.log(config + ' is absent in tests');
     absent = true;
   }
 });
 
-tests.some(function (test) {
+folders.some(function (test) {
   if (configs.indexOf(test) < 0) {
     console.log(test + ' is absent in dictionary');
     absent = true;
